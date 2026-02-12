eb253e2af468499f0b600ae47f106e9a66c47d44
Setyaaji <setyaji.angga@gmail.com>
2026-02-12 19:01:17 +0700

    deploy

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Installing dependencies..."
+echo "Installing npm dependencies..."
 npm install
 
 echo "Building Vite assets..."
 npm run build
 
-echo "Installing Composer dependencies..."
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install --no-dev --prefer-dist --optimize-autoloader --no-scripts
-rm composer.phar
-
 echo "Build complete!"
+
