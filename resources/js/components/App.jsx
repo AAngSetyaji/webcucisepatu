54556b71b0feea8e8876f2673f49ba2aa0100454
Aang Setyaji Priri Putra Pratama <57163158+AAngSetyaji@users.noreply.github.com>
2026-02-14 15:14:45 +0000

    add @vercel/analytics dependency and integrate Analytics component

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -3,10 +3,12 @@ import Navbar from './Navbar';
 import Main from './Main';
 import Footer from './Footer';
 import FloatingButtons from './FloatingButtons';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
     return (
         <div className="font-sans antialiased bg-slate-50 text-slate-900 selection:bg-brand-500 selection:text-white">
+            <Analytics />
             <Navbar />
             <Main />
             <Footer />
